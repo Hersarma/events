57eed4007a04f0c52997b4753ecd540f586cc220
Hersarma <hersarma@gmail.com>
2026-02-19 20:59:33 +0100

    d

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,12 +4,19 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta property="og:title" content="{{ $event->title ?? 'Diana’s Garden Studio' }}">
+        <meta property="og:description" content="Digitalna pozivnica za vaš poseban dan">
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="{{ url()->current() }}">
+        <meta property="og:image" content="{{ asset('images/logo2.png') }}">
 
         <title>{{ config('app.name', 'Invites') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="icon" type="image/png" href="{{ asset('images/logo2.png') }}">
+
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
