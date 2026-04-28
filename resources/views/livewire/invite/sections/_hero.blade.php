5fd2b4207a91709326f530936b778744cb58eea2
hersarma <hersarma@gmail.com>
2026-04-28 11:36:13 +0200

    s

--- resources/views/livewire/invite/sections/_hero.blade.php
+++ resources/views/livewire/invite/sections/_hero.blade.php
@@ -63,14 +63,18 @@
 
   {{-- Scroll hint --}}
   <div
-    x-show="opened"
-    x-transition.opacity
-    class="pointer-events-none absolute bottom-20 left-0 right-0 flex justify-center"
-  >
-    <svg class="h-8 w-8 animate-bounce text-white/90" viewBox="0 0 24 24" fill="none" aria-hidden="true">
-      <path d="M6 9l6 6 6-6" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round" />
-    </svg>
-  </div>
+  x-show="opened && afterFirst"
+  x-transition.opacity
+  class="pointer-events-none absolute bottom-20 left-0 right-0 flex flex-col items-center justify-center"
+>
+  <span class="mb-1 block text-xs font-bold uppercase tracking-[0.25em] text-white/80">
+  scroll
+</span>
+
+  <svg class="h-8 w-8 animate-bounce text-white/90" viewBox="0 0 24 24" fill="none" aria-hidden="true">
+    <path d="M6 9l6 6 6-6" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+  </svg>
+</div>
 
   {{-- klik bilo gde --}}
   <div x-show="requiresClick && !opened" @click.stop.prevent="open()" class="absolute inset-0">
